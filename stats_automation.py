838334989a939d3b7876a643f96f1d0b4df0eb92
Jared Wood <jared.wood135@gmail.com>
2023-03-09 10:43:22 -0600

    Completed 'T-Test' functions

--- stats_automation.py
+++ stats_automation.py
@@ -8,11 +8,11 @@ D 2. Imports
 D 3. Correlation
 D    1. PearsonR
 D    2. Spearman's
-4. T-Test
-    1. 1 Sample
-    2. 2 Sample
-    3. Values
-5. Significance
+D4. T-Test
+D    1. 1 Sample
+D    2. 2 Sample
+D    3. Values
+#5. Significance
     1. 3+ Sample
 6. Chi
     1. Contingency
@@ -50,8 +50,14 @@ from scipy import stats
 # =======================================================================================================
 
 def correlation_pearson(data, x_str, y_str):
+    '''
+    Returns a correlation and p-value as well as a visualization using the pearson methodology
+    (pearsonr used for straight line correlation)
+    stats.pearsonr(mpg.year, mpg.cty)
+    Tests the relationship between car year and city mileage
+    '''
     corr, pval = stats.pearsonr(data[x_str], data[y_str])
-    print(f'\033[32mCorrelation=\033[0m {corr}\n\033[32mP-value=\033[0m {pval}')
+    print(f'\033[32mCorrelation =\033[0m {corr}\n\033[32mP-value =\033[0m {pval}')
     sns.scatterplot(data[x_str], data[y_str])
     plt.title(f'Relationship between {x_str} and {y_str}')
     plt.show()
@@ -63,8 +69,14 @@ def correlation_pearson(data, x_str, y_str):
 # =======================================================================================================
 
 def correlation_spearman(data, x_str, y_str):
+    '''
+    Returns a correlation and p-value as well as a visualization using the spearman methodology
+    (spearmanr used for non-straight line correlation like a parabola or exponential growth)
+    stats.spearmanr(mpg.displ, mpg.cyl)
+    Tests the correlation between displ and cyl
+    '''
     corr, pval = stats.spearmanr(data[x_str], data[y_str])
-    print(f'\033[32mCorrelation=\033[0m {corr}\n\033[32mP-value=\033[0m {pval}')
+    print(f'\033[32mCorrelation =\033[0m {corr}\n\033[32mP-value =\033[0m {pval}')
     sns.scatterplot(data[x_str], data[y_str])
     plt.title(f'Relationship between {x_str} and {y_str}')
     plt.show()
@@ -75,7 +87,20 @@ def correlation_spearman(data, x_str, y_str):
 # T-Test(1-Sample) START
 # =======================================================================================================
 
-# stats.ttest_1samp()
+def ttest1samp(data, subset, setColStr):
+    '''
+    Returns a statistic and p-value as well as a visualization using the T-Test, 1-Sample methodology
+    (ttest_1samp is used for comparing a subset against the entire population)
+    stats.ttest_1samp(mpg[mpg.year == 1999].cty, mpg.cty.mean())
+    Tests the relationship of city mileage of 1999 cars to the city mileage of all cars
+    '''
+    stat, pval = stats.ttest_1samp(subset, data[setColStr].mean())
+    print(f'\033[32mStatistic =\033[0m {stat}\n\033[32mP-value =\033[0m {pval}')
+    sns.distplot(subset, label='Subset')
+    sns.distplot(data[setColStr], label='Overall')
+    plt.title(f'Overall {setColStr} Distribution')
+    plt.legend()
+    plt.show()
 
 # =======================================================================================================
 # T-Test(1-Sample) END
@@ -83,7 +108,20 @@ def correlation_spearman(data, x_str, y_str):
 # T-Test(2-Sample) START
 # =======================================================================================================
 
-# stats.ttest_ind()
+def ttestind(subset_var1, subset_var2):
+    '''
+    Returns a statistic and p-value as well as a visualization using the T-Test, independent methodology
+    (ttest_ind is used for comparing two independent subsets and a shared value)
+    stats.ttest_ind(mpg[mpg.year == 1999].cty, mpg[mpg.year == 2008].cty)
+    Tests the relationship of city mileage between 1999 and 2008 vehicles
+    '''
+    stat, pval = stats.ttest_ind(subset_var1, subset_var2)
+    print(f'\033[32mStatistic =\033[0m {stat}\n\033[32mP-value =\033[0m {pval}')
+    sns.distplot(subset_var1, label='Subset 1')
+    sns.distplot(subset_var2, label='Subset 2')
+    plt.title(f'Relationship between {subset_var1} and {subset_var2}')
+    plt.legend()
+    plt.show()
 
 # =======================================================================================================
 # T-Test(2-Sample) END
@@ -91,7 +129,23 @@ def correlation_spearman(data, x_str, y_str):
 # T-Test(Values) START
 # =======================================================================================================
 
-# stats.ttest_ind_from_values()
+def ttestval(mean1, std1, trials1, mean2, std2, trials2):
+    '''
+    Returns a statistic and p-value as well as a visualization using the T-Test, value methodology
+    (ttest_ind_from_stats is used for comparing two independent subsets using known values)
+    stats.ttest_ind_from_stats(90, 15, 50, 150, 10, 50)
+    Tests the relationship of the first independent value of {mean:90, std:15, trial:50}
+    and the second independent value of {mean:150, std:10, trial:50}
+    '''
+    stat, pval = stats.ttest_ind_from_stats(mean1, std1, trials1, mean2, std2, trials2)
+    print(f'\033[32mStatistic =\033[0m {stat}\n\033[32mP-value =\033[0m {pval}')
+    value1 = np.random.normal(mean1, std1, trials1)
+    value2 = np.random.normal(mean2, std2, trials2)
+    sns.distplot(value1, label='Value 1')
+    sns.distplot(value2, label='Value 2')
+    plt.title(f'Relationship between Value 1 and Value 2')
+    plt.legend()
+    plt.show()
 
 # =======================================================================================================
 # T-Test(Values) Sample END
